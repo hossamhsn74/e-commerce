5a448ed66d56d7d83ec1c5fa0a59cea6fa498b8b
hossam74 <hossamhsn74@gmail.com>
2020-02-06 03:15:02 +0200

    products details done

--- products/urls.py
+++ products/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from products import views
 urlpatterns = [
     path('', views.productHome),
+    path("<int:id>",views.details, name="details"),
+
 ]
--- products/views.py
+++ products/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from .models import products
+from .models import CATEGORY_CHOISE
 
 
 def productHome(request):
-    data = products.objects.all
-    return render(request, 'products/product-home.html', {'products': data})
+    data = products.objects.all()
+    all_products = {"products": data}
+    return render(request, 'products/product-home.html', context=all_products)
+
+
+def details(request, id):
+    for product in products.objects.all():
+        if product.id == id:
+            current_product = product
+    return render(request, 'products/product-details.html', context={"current_product": current_product})
